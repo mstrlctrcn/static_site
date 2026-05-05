d19055dfb656eecdfd96f8d4538a7775867a93e8
S Murphy <smurphy11work@gmail.com>
2026-05-05 13:23:15 -0400

    5-5-26 Completed tests for image and markdown link extraction

--- src/split_nodes.py
+++ src/split_nodes.py
@@ -1,4 +1,5 @@
 from textnode import TextType, TextNode
+import re
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
 	#Takes a list of old_nodes searches for the delimiter to find where text type changes.
 	# Create new nodes separating out the sections to be turned into a separate node of a different text type
@@ -30,3 +31,21 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
 		else:
 			new_nodes.append(node)
 	return new_nodes
+
+def extract_markdown_images(text):
+	images = re.findall(r"!\[.*?\)", text)
+	final_images = []
+	for image in images:
+		link = re.findall(r"\((.*?)\)", image)
+		alt_text = re.findall(r"!\[(.*?)\]", image)
+		final_images.append((alt_text[0], link[0]))
+	return final_images
+
+def extract_markdown_links(text):
+	links = re.findall(r"\[.*?\].*?\(.*?\)", text)
+	final_links = []
+	for link in links:
+		link_text = re.findall(r"\[(.*?)].*?\(.*?\)", link)
+		link_address = re.findall(r"\[.*?\].*?\((.*?)\)", link)
+		final_links.append((link_text[0], link_address[0]))
+	return final_links
--- src/test_split_nodes.py
+++ src/test_split_nodes.py
@@ -1,5 +1,5 @@
 import unittest
-from split_nodes import split_nodes_delimiter
+from split_nodes import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 from textnode import TextNode, TextType
 
 class Test_Split_Nodes(unittest.TestCase):
@@ -23,3 +23,24 @@ class Test_Split_Nodes(unittest.TestCase):
 		self.assertEqual(new_node_list[2].text, " embedded ")
 		self.assertEqual(new_node_list[3].text_type, TextType.ITALIC)
 		self.assertEqual(new_node_list[3].text, "and another")
+	def test_two_code_sections(self):
+		text_node = TextNode("Test with ```code section``` embedded ```and another```", TextType.PLAIN)
+		new_node_list = split_nodes_delimiter([text_node], "```", TextType.CODE)
+		self.assertEqual(new_node_list[0].text, "Test with ")
+		self.assertEqual(new_node_list[0].text_type, TextType.PLAIN)
+		self.assertEqual(new_node_list[1].text, "code section")
+		self.assertEqual(new_node_list[1].text_type, TextType.CODE)
+		self.assertEqual(new_node_list[2].text_type, TextType.PLAIN)
+		self.assertEqual(new_node_list[2].text, " embedded ")
+		self.assertEqual(new_node_list[3].text_type, TextType.CODE)
+		self.assertEqual(new_node_list[3].text, "and another")
+	def test_image_extraction(self):
+		text_node = TextNode("Test with image ![alt text for image](www.test.com) embedded", TextType.PLAIN)
+		image_list = extract_markdown_images(text_node.text)
+		self.assertEqual(image_list[0][0], "alt text for image")
+		self.assertEqual(image_list[0][1], "www.test.com")
+	def test_link_extraction(self):
+		text_node = TextNode("Test with [link](www.test.com) embedded", TextType.PLAIN)
+		link_list = extract_markdown_links(text_node.text)
+		self.assertEqual(link_list[0][0], "link")
+		self.assertEqual(link_list[0][1], "www.test.com")
